48b1c40a737ce8eda979c46a72a979b0299ca5a3
Miguel Angel <msanchez@wepsys.com>
2023-03-08 16:03:56 -0400

    install the package and create structure file

---
+++ CA.MigratorDB/Main/ConnectionStringCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.MigratorDB.Main
+{
+    [Serializable]
+    public class ConnectionStringCollection
+    {
+        public string ConnectionStringSqlServer { get; set; } = string.Empty;
+        public string ConnectionStringMySql { get; set; } = string.Empty;
+
+    }
+}

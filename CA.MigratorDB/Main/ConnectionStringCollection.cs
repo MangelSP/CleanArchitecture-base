0c19d9a4ab8526b3f279eaafdbda64d2e03fbaa7
Miguel Angel <msanchez@wepsys.com>
2023-03-08 16:16:01 -0400

    added the class and implementation

--- CA.MigratorDB/Main/ConnectionStringCollection.cs
+++ CA.MigratorDB/Main/ConnectionStringCollection.cs
@@ -9,7 +9,7 @@ namespace CA.MigratorDB.Main
     [Serializable]
     public class ConnectionStringCollection
     {
-        public string ConnectionStringSqlServer { get; set; } = string.Empty;
+        public string ConnectionStringSQLServer { get; set; } = string.Empty;
         public string ConnectionStringMySql { get; set; } = string.Empty;
 
     }
--- CA.MigratorDB/Main/Program.cs
+++ CA.MigratorDB/Main/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CA.MigratorDB;
+using CA.MigratorDB.Main;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var services = ConfigureServices();
+        var serviceProvider = services.BuildServiceProvider();
+        await serviceProvider.GetService<App>().RunAsync(args);
+    }
+
+    public static IConfiguration LoadConfiguration()
+    {
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                .AddJsonFile("AppSettings.json", optional: true, reloadOnChange: true);
+        return builder.Build();
+    }
+
+    private static IServiceCollection ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        var config = LoadConfiguration();
+        services.AddSingleton(config);
+
+        /* Lectura de opciones del archivo de configuración. */
+        services.Configure<ConnectionStringCollection>(options => config.GetSection($"CollectionConnectionStrings").Bind(options));
+
+        /* Inyectamos la clase 'App' */
+        services.AddSingleton<App>();
+
+        /* Otros servicios de la aplicación de la consola. */
+        //services.AddTransient<IUser, User>();
+
+        return services;
+    }
+}
